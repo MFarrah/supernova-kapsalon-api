39faa0988c9512e4736706c29a4f5b4cac5e2fec
MFarr <micfar85@gmail.com>
2024-04-04 08:27:46 +0200

    postgresql database connection configured

---
+++ src/main/java/nl/mfarr/supernovakapsalonapi/SupernovaKapsalonApiApplication.java
@@ -0,0 +1,13 @@
+package nl.mfarr.supernovakapsalonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupernovaKapsalonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupernovaKapsalonApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/mfarr/supernovakapsalonapi/SupernovaKapsalonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mfarr.supernovakapsalonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupernovaKapsalonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
